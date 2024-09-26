a60729ed05a4a771db86a5543a0da0d5ac4be173
codexuz <142141576+codexuz@users.noreply.github.com>
2024-09-26 16:49:29 +0500

    Update SpeakingCard.vue

--- src/components/SpeakingCard.vue
+++ src/components/SpeakingCard.vue
@@ -11,11 +11,11 @@
           <ion-icon :icon="micSharp" class="w-10 h-10 text-[#0075e1]"></ion-icon>
           <br />
           <ion-text color="dark">Speaking Section</ion-text>
-          <ion-text color="dark"><b>5-6 Questions</b></ion-text>
+          <ion-text color="dark"><b>7-8 Questions</b></ion-text>
         </div>
         <ul class="list-disc p-9">
           <li>The virtual examiner asks questions about yourself and your life.</li>
-          <li>You will be given 5 seconds for preparation and 1 minute to speak for each question.</li>
+          <li>You will be given 40 seconds to speak for each question.</li>
         </ul>
         <div class="flex items-center justify-center w-full mb-4">
           <ion-button @click="startSpeaking" expand="block" shape="round">Start Speaking</ion-button>
@@ -37,14 +37,20 @@
         </div>
         
         <!-- Audio Visualization -->
-        <div class="flex flex-col items-center justify-center w-full">
+        <div v-if="showBar" class="flex flex-col items-center justify-center w-full">
           <AVMedia :media="stream" type="frequ" :frequ-lnum="6" :frequ-line-cap="true" :line-width="2" :canv-width="30" :canv-height="30" line-color="#00ff00"/>
         </div>
 
         <!-- Ready Button and Timer -->
-        <div class="w-full flex items-center justify-center mb-3 mt-4">
+        <div class="w-full flex items-center justify-center gap-x- mb-3 mt-4">
           <ion-button v-if="showReadyButton" shape="round" @click="onReadyClick">I'm Ready</ion-button>
-          <ion-button v-else shape="round">{{ time < 10 ? `00: 0${time}`: `00: ${time}` }}</ion-button>
+          <ion-button color="danger" v-if="showTimerButton" shape="round">
+            <div class="w-3 h-3 bg-white mr-2 rounded-full"></div>
+            {{ time < 10 ? `00: 0${time}`: `00: ${time}` }}
+          </ion-button>
+          <button @click="skipQuestion" v-if="skipButton">
+            <svg class="size-10 text-blue-600" xmlns="http://www.w3.org/2000/svg"  viewBox="0 0 16 16"><path fill="currentColor" d="M6.22 3.22a.75.75 0 0 1 1.06 0l4.25 4.25a.75.75 0 0 1 0 1.06l-4.25 4.25a.75.75 0 0 1-1.042-.018a.75.75 0 0 1-.018-1.042L9.94 8L6.22 4.28a.75.75 0 0 1 0-1.06"/></svg>
+          </button>
         </div>
       </ion-card-content>
     </ion-card>
@@ -55,8 +61,9 @@
 import { ref, onMounted, useTemplateRef } from 'vue';
 import { IonCard, IonCardContent, IonCardHeader, IonCardTitle, IonCardSubtitle, IonText, IonIcon, IonButton } from '@ionic/vue';
 import { micSharp } from 'ionicons/icons';
-import { useSpeechRecognition, useUserMedia } from '@vueuse/core';
 import { AVMedia } from 'vue-audio-visual';
+import { RecordRTCPromisesHandler, getBlob } from 'recordrtc';
+
 
 // Media and Assets
 import Poster from '@/assets/audios/poster.png';
@@ -70,9 +77,6 @@ import Video6 from '@/assets/audios/6.mp4';
 import Video7 from '@/assets/audios/7.mp4';
 import Video8 from '@/assets/audios/8.mp4';
 
-// Use User Media (Microphone)
-const { stream, enabled } = useUserMedia({ constraints: { audio: true } });
-
 
 
 // State Variables
@@ -80,8 +84,13 @@ const isSpeakingStart = ref(false);
 const currentIndex = ref(0);
 const videoRef = useTemplateRef('video')
 const speakingText = ref('');
-const showReadyButton = ref(true);
-const time = ref(60);
+const showReadyButton = ref(false);
+const time = ref(40);
+const showBar = ref(false)
+const isExamStarted = ref(false)
+const showTimerButton = ref(false);
+let stream = ref(null)
+let recorder = ref(null)
 
 // Speaking Questions with Videos
 const speakingQuestions = ref([
@@ -105,8 +114,7 @@ function startSpeaking() {
 // Ready Button Click: Start Recording and Timer
 function onReadyClick() {
   showReadyButton.value = false; // Hide the "I'm Ready" button
-  startRecording(); // Start speech recognition
-  startTimer(); // Start countdown timer
+  nextQuestion();
 }
 
 // Load and Play the Next Video
@@ -116,7 +124,14 @@ function loadNextVideo() {
     speakingText.value = speakingQuestions.value[currentIndex.value].text;
     videoRef.value.play();
     videoRef.value.onended = () => {
-      showReadyButton.value = true; // Show "I'm Ready" button after video ends
+      if(!isExamStarted.value){
+         showReadyButton.value = true; // Show "I'm Ready" button after video ends
+         isExamStarted.value = true
+      }
+      else{
+        startRecording()
+        startTimer()
+      }
     };
   }
 }
@@ -127,11 +142,22 @@ function nextQuestion() {
   loadNextVideo();
 }
 
+const skipButton = ref(false)
+function skipQuestion () {
+  skipButton.value = false
+  showTimerButton.value = false;
+  nextQuestion();
+  stopTimer()
+}
+
 // Countdown Timer and Stop Recording
 let interval;
 function startTimer() {
-  time.value = 60;
+  time.value = 40;
   interval = setInterval(() => {
+    if(time.value === 30){
+      skipButton.value = true
+    }
     if (time.value > 0) {
       time.value--;
     } else {
@@ -148,15 +174,25 @@ function stopTimer() {
 }
 
 
-function startRecording () {
-  enabled.value = true
+async function startRecording () {
+  stream.value = await navigator.mediaDevices.getUserMedia({audio: true});
+   recorder.value = new RecordRTCPromisesHandler(stream.value, {
+    type: 'audio/webm'
+   });
+  showBar.value = true;
+  showTimerButton.value = true;
+  recorder.value.startRecording();
+
 }
 
-function stopRecording () {
-  enabled.value = false
+async function stopRecording () {
+  showBar.value = false;
+  showTimerButton.value = false;
+  let blob = recorder.value.stopRecording();
+  console.log(blob)
 }
 
-onMounted(() => {
+onMounted( async() => {
   // Cleanup when component unmounts
   return () => {
     clearInterval(interval);
